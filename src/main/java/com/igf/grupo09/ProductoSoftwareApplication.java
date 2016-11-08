340022b34a275203b6fc9e559fcfdc3d83d1cf86
Salvador Chacón <chamba.chs.90@gmail.com>
2016-11-07 18:04:40 -0600

    Inicialización de estructura de proyecto

---
+++ src/main/java/com/igf/grupo09/ProductoSoftwareApplication.java
@@ -0,0 +1,12 @@
+package com.igf.grupo09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoSoftwareApplication.class, args);
+	}
+}
